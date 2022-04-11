e34d29fb78e048a37c5c03c42a3d144a428ec5e5
kmclin4 <98778367+kmclin4@users.noreply.github.com>
2022-04-11 16:05:06 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -7,4 +7,10 @@ var radarDisplayOptions = {
   format: 'image/png',
   transparent: true
 };
+
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+
+var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+$.getJSON(weatherAlertsUrl, function(data) {
+  L.geoJSON(data).addTo(map);
+});
