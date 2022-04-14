f349a6ec17b29b284bfb6fdc6b28d9905713d93a
kmclin4 <98778367+kmclin4@users.noreply.github.com>
2022-04-13 23:10:50 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([38, -95], 4);
+var map = L.map('map').setView([37, -97], 4);
 var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
